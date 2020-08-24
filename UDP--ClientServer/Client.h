30e96a4ac2b754c8776819310ef350bcd2d2b1e1
Lidor Sommer <lidorso@mta.ac.il>
2020-08-24 16:52:58 +0300

    Initial commit

---
+++ UDP--ClientServer/Client.h
@@ -0,0 +1,8 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h> 
+#include <string.h>
+
+#define TIME_PORT	27015
